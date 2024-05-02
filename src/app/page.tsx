7e42b34d04f52339f7f93c0fc93c2e9de7f7600d
Zerguiniali <zerguini.alilo@gmail.com>
2024-05-02 14:27:01 +0100

    change 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
 import AccueilImagesCarousel from "@/components/AccueilImagesCarousel";
+import APCDALS from '../../public/APCDALs.png'
+import baosem from '../../public/baosem.png'
+import bomop from '../../public/bomop.png'
+import logo from '../../public/Logo_du_ministre_de_la_dfense_nationale2.png'
+import press from '../../public/presse.jpg'
+import news from '../../public/newpaper.jpg'
 
 export default function Home() {
   return (
-   <div className="overflow-hidden px-4">
+   <div className="overflow-hidden">
     <div className="flex flex-col justify-center items-center md:px-8 lg:px-10 py-10 h-[100vh]">
       <AccueilImagesCarousel/>
     </div>
@@ -11,6 +17,22 @@ export default function Home() {
       <h1 className="text-4xl font-bold text-center">Qui sommes-nous ?</h1>
       <p className="w-full md:w-2/3 py-4 text-xl text-center">Nous sommes une plateforme électronique dédiée aux avis d'appels d'offres et consultations publics et privés en Algérie. Notre engagement consiste à agréger, analyser, et valoriser des données précieuses pour vous.</p>
       <p className="w-full md:w-2/3 py-4 text-xl text-center">En plus de cela, nous vous offrons la possibilité de trouver les partenaires idéaux - collaborateurs, donneurs d'ordres, sous-traitants, fournisseurs et investisseurs - pour saisir ensemble des opportunités lucratives. Rejoignez-nous et prospérez grâce à des collaborations harmonieuses !</p>
+    </div>
+    <div className="flex flex-col justify-center items-center px-5 md:px-8 py-20 lg:px-10 pb-20 relative">
+    <Image src={news} alt="" layout="fill" objectFit="cover" className="bg-black opacity-70"/>
+      <h1 className="text-4xl font-bold text-center z-20">Nos sources d'information</h1>
+      <p className="w-full md:w-2/3 py-4 text-xl text-center z-20">
+      Avec notre service, vous ne manquerez aucun marché pertinent grâce à une analyse quotidienne de centaines de sources, telles que la presse algérienne, les consultations de plusieurs APC et Wilayas, ainsi que différentes directions, en plus des revues spécialisées telles que le Bulletin Officiel des Marchés de l'Opérateur Public (BOMOP) et le Bulletin des Appels d'Offres du Secteur de l'Énergie et des Mines (BAOSEM), ainsi que la consultation des appels d’offres exclusifs de nos partenaires. Pour une prise de décision rapide et efficace, notre service rassemble les informations provenant de multiples sources pour enrichir vos données et éviter les doublons.
+      </p>
+      <div className="flex flex-wrap justify-center items-center gap-10 relative">
+  <Image alt="" src={press} className="w-52 max-w-full"/>
+  <Image alt="" src={logo} className="w-28 max-w-full"/>
+  <Image alt="" src={APCDALS} className="w-28 max-w-full"/>
+  <Image alt="" src={baosem} className="w-28 max-w-full"/>
+  <Image alt="" src={bomop} className="w-28 max-w-full"/>
+</div>
+
+
     </div>
    </div>
   );
--- src/components/AccueilImagesCarousel.tsx
+++ src/components/AccueilImagesCarousel.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Image from 'next/image';
-import cahier from '../../public/cahier.jpg';
-import planet from '../../public/planet.jpg';
-import coffe from '../../public/coffe.jpg';
-import sahm from '../../public/sahm.jpg';
+import cahier from '../../public/folder.jpg';
+import planet from '../../public/sahres.jpg';
+import coffe from '../../public/desktop.jpg';
+import sahm from '../../public/shares2.jpg';
 import {
     Carousel,
     CarouselContent,
@@ -14,37 +14,36 @@ import {
 
 const AccueilImagesCarousel = () => {
     return (
-        <Carousel className="w-full h-full">
+        <Carousel className="w-full h-full rounded-lg">
             <CarouselContent>
                 <CarouselItem>
-                    <div className="h-screen relative overflow-hidden">
+                    <div className="h-screen relative overflow-hidden rounded-lg">
                         <Image alt="" src={planet} layout="fill" objectFit="cover" />
-                        <div className="absolute inset-0 flex flex-col justify-center items-center text-white text-center">
-                            <h1 className="text-xl md:text-2xl lg:text-4xl font-bold mb-4 w-2/3">Votre plateforme d’appels d’offres et d’opportunités business</h1>
+                        <div className="absolute inset-0 flex flex-col justify-center items-center text-white text-center bg-black opacity-80">
+                            <h1 className="text-xl md:text-2xl lg:text-5xl font-bold mb-4 w-2/3 ">Votre plateforme d’appels d’offres et d’opportunités business</h1>
                         </div>
                     </div>
                 </CarouselItem>
                 <CarouselItem>
-                    <div className="h-screen relative overflow-hidden">
+                    <div className="h-screen relative overflow-hidden rounded-lg">
                         <Image alt="" src={cahier} layout="fill" objectFit="cover" />
-                        <div className="absolute inset-0 flex flex-col justify-center items-center text-black text-center">
+                        <div className="absolute inset-0 flex flex-col justify-center items-center text-white text-center bg-black opacity-80">
                             <h1 className="text-xl md:text-2xl lg:text-4xl font-bold mb-4 w-2/3">Un Accompagnement pour tout retrait de cahier de charge, dépôt de soumission et ouverture des plis</h1>
                         </div>
                     </div>
                 </CarouselItem>
                 <CarouselItem>
-                    <div className="h-screen relative overflow-hidden">
+                    <div className="h-screen relative overflow-hidden rounded-lg">
                         <Image alt="" src={sahm} layout="fill" objectFit="cover" />
-                        <div className="absolute inset-0 flex flex-col justify-center items-center text-white text-center">
-                            <h1 className="text-xl md:text-2xl lg:text-4xl font-bold mb-4 w-2/3">Vous cherchez un partenaire de confiance pour votre entreprise ?
-Ms marches est votre meilleure solution</h1>
+                        <div className="absolute inset-0 flex flex-col justify-center items-center text-white text-center bg-black opacity-80">
+                            <h1 className="text-xl md:text-2xl lg:text-4xl font-bold mb-4 w-2/3">Vous recherchez un partenaire de confiance pour votre entreprise ? Nous sommes votre meilleure solution.</h1>
                         </div>
                     </div>
                 </CarouselItem>
                 <CarouselItem>
-                    <div className="h-screen relative overflow-hidden">
+                    <div className="h-screen relative overflow-hidden rounded-lg">
                         <Image alt="" src={coffe} layout="fill" objectFit="cover" />
-                        <div className="absolute inset-0 flex flex-col justify-center items-center text-white text-center">
+                        <div className="absolute inset-0 flex flex-col justify-center items-center text-white text-center bg-black opacity-80">
                             <h1 className="text-xl  md:text-4xl lg:text-4xl font-bold mb-4 w-2/3">votre meilleur partenaire en service après vente, veille à apporter un suivi de qualité à ses abonnés.</h1>
                         </div>
                     </div>
